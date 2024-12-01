2868298ae0565b82ace2ea68ff020f269aaa133d
_b.romulo <68164627+RomuloBreno@users.noreply.github.com>
2024-12-01 15:11:18 -0300

    fix bug notify add new view and new notify anytime in view

--- src/components/notify/Notify.jsx
+++ src/components/notify/Notify.jsx
@@ -1,40 +1,76 @@
 import React, { useEffect, useState } from 'react';
-import { useAuth } from '../../context/authContext';
+import { ENotify } from '../../enumNotify';
+import { fetchApi } from '../../utils/fetch';
+import { Link } from 'react-router-dom';
+import { formatDateToNotify } from '../../utils/formatText';
 
-const Notify = ({indexParam,messageParam}) => {
-    const [message, setMessage] = useState(messageParam);
-    const [index, seIndex] = useState(indexParam);
+const Notify = ({indexParam,messageParam, dataToken}) => {
+    // const [message, setMessage] = useState(messageParam);
+    const [index, setIndex] = useState(indexParam);
+    const [user, setUser] = useState();
+    const {notifier, message, postId, creationDate} = messageParam
     useEffect(() => {
-    }, [message]);
+        const fetchUser = async () => {
+            const response = await fetchApi(`v1/user/${notifier}`, null, "GET", null, dataToken);
+            if (response.status) setUser(response.result);
+          };
+          fetchUser()
+    }, [messageParam]);
+    useEffect(() => {
+    }, [message, user]);
    
       return (
-        <div key={index} style={style.notificationStyles}>
-          <div style={style.iconStyles}>
-            <i className="fas fa-bell"></i>
-          </div>
-          <div style={style.messageStyles}>
-            AAAAAAAAAAAAA
-          </div>
-          <button
-            style={style.closeButtonStyles}
-            onClick={style.onClose}
-          >
-            &times;
-          </button>
+        <div style={{cursor: 'pointer' }}>
+        {message &&
+            <div style={style.notificationStyles}>
+              <div style={style.iconStyles}>
+                <i className="fas fa-bell"></i>
+              </div>
+              <div style={style.messageStyles}>
+              <span style={{fontWeight:'400'}}>
+                <Link to={`/profile/${user?.nick}`}  style={{ textDecoration:'none'}}>
+                 {user?.nick + " "} 
+                </Link>
+                </span>
+                {postId 
+                ? (
+                    <>
+                <Link to={`/post/${postId}`} style={{ textDecoration:'none', color:'grey'}}>
+                   {typeof message == "number" ? ENotify[message] : message}
+                </Link>
+                    </>
+                )
+                : (
+                    <Link to={`/profile/${user?.nick}`} style={{ textDecoration:'none', color:'grey'}}>
+                    {typeof message == "number" ? ENotify[message] : message}
+                    </Link>
+                )
+                }
+                <span>{ "   " +formatDateToNotify(creationDate)}</span>
+              </div>
+              {/* <button
+                style={style.closeButtonStyles}
+                onClick={style.onClose}
+              >
+                &times;
+              </button> */}
+            </div>
+            }
         </div>
       );
     };
     const style ={
       notificationStyles : {
-        //   display: 'flex',
-        //   alignItems: 'center',
-        //   backgroundColor: '#fff',
-        //   padding: '12px 16px',
-        //   borderRadius: '8px',
-        //   boxShadow: '0 2px 6px rgba(0, 0, 0, 0.1)',
-        //   width: '300px',
-        //   margin: '10px',
-        //   position: 'relative',
+          display: 'flex',
+          alignItems: 'center',
+          backgroundColor: '#fff',
+          padding: '12px 16px',
+          borderRadius: '8px',
+          boxShadow: '0 2px 6px rgba(0, 0, 0, 0.1)',
+          width: 'auto',
+          margin: '10px',
+          position: 'relative',
+          cursor: 'pointer' 
         },
       
         iconStyles : {
---
+++ src/enumNotify.js
@@ -0,0 +1,11 @@
+export const ENotify = {
+   1: "Curtiu sua publicação",
+   2: "Comentou na sua publicação",
+   3: "Compartilhou sua publicação",
+   4: "Te mencionou em uma publicação",
+   5: "Te enviou uma solicitação de amizade",
+   6: "Aceitou sua solicitação de amizade",
+   7: "Respondeu ao seu comentário",
+   8: "Te seguiu",
+   9: "Deixou de te seguir",
+ };
